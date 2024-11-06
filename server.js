9578fa0922032c3be1999b9d04ab6df9ab262aa8
Stanley Ifeanyi <ifeanyistanley82@gmail.com>
2024-11-06 21:31:50 +0100

    edited time tracking and task cntrl

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const dotenv = require(`dotenv`).config()
 const bcrypt = require('bcrypt')
 
   
-const Task = require("./models/Task");
-const Comment = require('./models/Comment');
+
+
 const connectToDatabase = require('./config/db');
 
 const taskRouter = require('./routes/taskRoute')
-//const Users = require('./models/User');
-//const sendUserEmail = require('./sendEmail'); 
+
 
 const app = express()
 app.use(express.json()); // Middleware to parse JSON bodies  
